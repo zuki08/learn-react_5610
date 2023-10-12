21f49c92da306632a8355d49391b08fd4faea8a9
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2023-10-12 15:50:21 -0400

    recipes

---
+++ src/pages/qcomps/recipes.js
@@ -0,0 +1,22 @@
+export const recipes = [{
+  id: 'greek-salad',
+  name: 'Greek Salad',
+  ingredients: new Set(['tomatoes', 'cucumber', 'onion', 'olives', 'feta'])
+}, {
+  id: 'hawaiian-pizza',
+  name: 'Hawaiian Pizza',
+  ingredients: new Set(['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'])
+}, {
+  id: 'hummus',
+  name: 'Hummus',
+  ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
+}];
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {}
+    </div>
+  );
+}
