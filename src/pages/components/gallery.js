c32518ed9cc379626f27a26f8700f4f0ae729a48
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2023-10-12 12:27:35 -0400

    part1 gallery

---
+++ src/pages/components/gallery.js
@@ -0,0 +1,12 @@
+import Profile from './profile.js'
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Profiles: </h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
+}
