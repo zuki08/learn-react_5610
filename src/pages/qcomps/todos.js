a2dc53200444c3429ad2c69c4bb318213440f278
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2023-10-12 13:34:21 -0400

    jsx qs

---
+++ src/pages/qcomps/todos.js
@@ -0,0 +1,22 @@
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 's',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>Person Name's Todos</h1>
+      <img className="avatar"
+      src="URL"
+      alt="Person's Name"
+      />
+    </div>
+  );
+}
