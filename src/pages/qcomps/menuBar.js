292d0c320130ccef82f1cb1506c3370ae96ea4b2
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-02-12 09:07:18 -0500

    edits

--- src/pages/qcomps/menuBar.js
+++ src/pages/qcomps/menuBar.js
@@ -1,22 +1,23 @@
 export default function MenuBar() {
   return (
     <div>
-      <AButton>
+      <AButton id="btn1" color="red" size="50px">
         Button 1
       </AButton>
-      <AButton>
+      <AButton id="btn2" color="green" size="30px">
         Button 2
       </AButton>
     </div>
   );
 }
 
-function AButton({ id, color, size, children }) {
+function AButton() {
+  const handleClick = () => {
+    document.getElementById(`${id}`).style.backgroundColor = color;
+    document.getElementById(`${id}`).style.fontSize = size;
+  }
   return (
-    <button id={`${id}`} onClick={() => {
-      document.getElementById(`${id}`).style.backgroundColor = color;
-      document.getElementById(`${id}`).style.fontSize = size;
-    }}>
+    <button id={`${id}`} onClick={handleClick}>
       {children}
     </button>
   );
