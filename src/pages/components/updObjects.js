275f5914cb72f795449aa99ac693a613c96c55ec
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-02-14 09:48:25 -0500

    formatting

--- src/pages/components/updObjects.js
+++ src/pages/components/updObjects.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 
 export default function Form() {
   const [person, setPerson] = useState({
-                                name: 'Niki de Saint Phalle',
-                                artwork: {
-                                  title: 'Blue Nana',
-                                  city: 'Hamburg',
-                                  image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-                                }
-                              });
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
+  });
 
   function handleCityChange(e) {
     const nextArtwork = { ...person.artwork, city: e.target.value };
